20605e631e1293e05b903c9a05dfe86d2a793f73
enricrocavilalta <93587910+enricrocavilalta@users.noreply.github.com>
2021-11-30 12:37:36 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+$int = -4;
+$double = 2.3;
+$string ="string";
+$bool=true;
+
+echo $int . "<br>" . $double . "<br>" . $string . "<br>" . $bool;
+?>
+
+</body>
+</html> 
